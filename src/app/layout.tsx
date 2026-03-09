43ce788525f77b6856fe5b70fd7c3c285a94f093
aisafarimarketing-ux <aisafarimarketing@gmail.com>
2026-03-09 09:17:21 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,23 @@
-import './globals.css'
-import type { Metadata } from 'next'
+import "./globals.css";
+import type { Metadata } from "next";
+import Navbar from "../components/Navbar";
 
 export const metadata: Metadata = {
-  title: 'Safari Trade Directory | 2025-2026',
-  description: 'Nyumbani & Hilala Camp Special Offers and Trade Profiles',
-}
+  title: "Safari Trade Directory | 2025-2026",
+  description: "Nyumbani & Hilala Camp Special Offers and Trade Profiles",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-neutral-950 text-white">
+        <Navbar />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
